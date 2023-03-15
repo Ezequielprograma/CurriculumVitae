2feaef1c974e7dec40fe6bbc21fe249d04322563
Ezequielprograma <ezequiel.medina.profesional@gmail.com>
2023-03-15 15:14:17 -0300

    foto y animacion

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,26 @@
 function desplegar(){
-    var el = document.getElementById("infoPersonal");
-    el.setAttribute("style","display:none;");
-    
+    if(num == false){
+        document.getElementById("infoPersonal").setAttribute("style","display:none;");
+        document.getElementById("conocimientos").setAttribute("style","display:none;");
+        document.getElementById("repositorio").setAttribute("style","display:none;");
+        document.getElementById("masSobreMi").setAttribute("style","display:none;");
+        
+        num = true;
+    }else{
+        document.getElementById("infoPersonal").setAttribute("style","display:grid;"); 
+        document.getElementById("conocimientos").setAttribute("style","display:grid;"); 
+        document.getElementById("repositorio").setAttribute("style","display:grid;"); 
+        document.getElementById("masSobreMi").setAttribute("style","display:grid;"); 
+        
+        num = false;
+    }
     
 }
+
+function correrFotoPerfil(){
+    //document.getElementById("fotoPerfil").setAttribute("style", "transition-property: width, height,top,left,display;transition-duration: 5s, 5s,5s,5s,5s;transition-timing-function: easy-in;width:150px; height:150px; display:flex; position:fixed; top:0px; left:0px;");
+  //document.getElementById("bienvenida").setAttribute("style","display:flex;");
+}
+
+var num = false;
+
